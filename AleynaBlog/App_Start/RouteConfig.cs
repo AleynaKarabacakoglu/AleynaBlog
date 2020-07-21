af49f1dc1935cef59c21ff86d49ae9e54609b599
Aleyna Karabacakoğlu <50250045+AleynaKarabacakoglu@users.noreply.github.com>
2020-07-21 22:41:54 +0300

    Add project files.

---
+++ AleynaBlog/App_Start/RouteConfig.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace AleynaBlog
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+               name: "About",
+               url: "hakkimda",
+               defaults: new { controller = "About", action = "Index" }
+           );
+
+            routes.MapRoute(
+               name: "Blog",
+               url: "blog",
+               defaults: new { controller = "Blog", action = "Index", id = UrlParameter.Optional }
+           );
+            routes.MapRoute(
+              name: "Contact",
+              url: "Iletisim",
+              defaults: new { controller = "Contact", action = "Index", id = UrlParameter.Optional }
+          );
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
---
+++ AleynaBlog/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AleynaBlog.MvcApplication" Language="C#" %>
