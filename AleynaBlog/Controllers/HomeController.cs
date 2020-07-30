1003a7e422e3cef9c023036e85e2c231f28733f9
Aleyna Karabacakoğlu <50250045+AleynaKarabacakoglu@users.noreply.github.com>
2020-07-30 16:05:56 +0300

    changed

--- AleynaBlog/Controllers/HomeController.cs
+++ AleynaBlog/Controllers/HomeController.cs
@@ -9,8 +9,6 @@ namespace AleynaBlog.Controllers
     using Models;
     public class HomeController : Controller
     {
-
-
         MyBlogEntities db = new MyBlogEntities();
         public ActionResult Index()
         {
